70f10d5439e93ecfb061689421c6cebb1848920f
王一阁 <1776686515@qq.com>
2023-05-23 17:34:55 +0800

    入口函数中背景图的webview和命令注册位置更改，资源校验完成后再进行注册

--- src/backgroundImage/regist.ts
+++ src/backgroundImage/regist.ts
@@ -18,15 +18,14 @@ export function registBackground (context: ExtensionContext) {
 	}).then(() => {
 		// 开启后判断是否随机修改背景
 		setRandomBackground();
+		// 设置背景图的侧栏webview注册
+		const backgroundWebview = registWebview('wangyige.custom.backgroundImage', new webviewCreateByHtml('webview/background', '背景图片'));
+		context.subscriptions.push(backgroundWebview);
+		// 命令事件注册
+		commands.registerCommand('wangyige.background.selectStore', selectFolderForBackgroundStore);
+		commands.registerCommand('wangyige.background.resetStore', resetBackgroundStorePath);
+		commands.registerCommand('wangyige.background.clear', clearBackgroundConfig);
 	});
-	// 设置背景图的侧栏webview注册
-	const backgroundWebview = registWebview('wangyige.custom.backgroundImage', new webviewCreateByHtml('webview/background', '背景图片'));
-	context.subscriptions.push(backgroundWebview);
-
-	// 命令事件注册
-	commands.registerCommand('wangyige.background.selectStore', selectFolderForBackgroundStore);
-	commands.registerCommand('wangyige.background.resetStore', resetBackgroundStorePath);
-	commands.registerCommand('wangyige.background.clear', clearBackgroundConfig);
 }
 
 /**

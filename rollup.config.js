b0cb3b362b9d72ef7a9cdf1dd8db5b7556131bed
王一阁 <1776686515@qq.com>
2023-08-04 15:56:02 +0800

    修改rollup配置，自定义插件修改打包过程中对package.json的相对引用路径

--- rollup.config.js
+++ rollup.config.js
@@ -23,6 +23,47 @@ module.exports = {
         resolve(),
         json(),
         commonjs(),
-        terser()
+        terser(),
+        changeRequire('..')
     ]
+}
+
+/**
+ * 将引用外部json文件的路径修改
+ * @param {string} root 根路径
+ * @returns 
+ */
+function changeRequire (root = '.') {
+    return {
+        name: 'changeRequire',
+        /** @param {string} code */
+        transform: function (code, id) {
+            try {
+                let regexp = /(require\s*\(\s*)(?:'([^']*\.json)'|"([^"]*\.json)")(\s*\))/;
+                if (regexp.test(code)) {
+                    let repRegexp = new RegExp(regexp, 'g');
+                    return code.replace(repRegexp, (s, start, $1, $2, end) => {
+                        // $1是匹配单引号，$2是匹配双引号
+                        let res = checkPosition($1??$2??'');
+                        return `${start}"${root}/${res}"${end}`;
+                    });
+                }
+            } catch (error) {
+                this.error({ message: 'Replace Require Path Error', id: id, cause: error });
+                return null;
+            }
+        }
+    }
+}
+
+/** @param {string} path */
+function checkPosition (path) {
+    if (!path) return '';
+    let parent = 0, paths = [];
+    path.split('/').forEach((item, index) => {
+        if (item === '.' && index > 0) throw new Error('Illegal Path');
+        else if (item === '..') parent++;
+        else paths.push(item);
+    });
+    return paths.join('/');
 }
--- src/version/index.ts
+++ src/version/index.ts
@@ -1,3 +1,6 @@
+/** 版本号，加载时直接解析json，避免多次调用 */
+const version = require('../../package.json').version as string;
+
 /**
  * 是否是开发环境，本地调试环境变量是development，生产环境没有NODE_ENV
  * @returns 
@@ -11,7 +14,5 @@ export function isDev (): boolean {
  * @returns {string} 版本号
  */
 export function getVersion (): string {
-    // 开发环境路径在两层根目录，生产环境合并在一个文件中所以往上一层
-    const json = isDev() ? require('../../package.json') : require('../package.json');
-    return json.version as string;
+    return version;
 }

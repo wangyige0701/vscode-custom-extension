6cb474d9d71854c3bd0d300cc9d55cab74968315
王一阁 <1776686515@qq.com>
2023-09-05 09:39:49 +0800

    配置文件修改

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,12 @@
 import type { ExtensionContext } from 'vscode';
 import { registBackground } from './background/regist';
 import { contextContainer } from './utils/webview/index';
-import { checksumsInit } from './utils/checksums';
 
 export function activate(context: ExtensionContext) {
 	contextContainer.instance = context;
 
 	// 注册背景图侧栏页面
 	registBackground();
-
-	checksumsInit();
 }
 
 // This method is called when your extension is deactivated

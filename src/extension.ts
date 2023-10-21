5b045e1913f4bf141a04a03f52f22c351b4b9236
王一阁 <1776686515@qq.com>
2023-10-21 19:54:46 +0800

    设置终止时间显示方法

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import { registBackground } from './app/background/regist';
 import { contextContainer } from './utils/webview/index';
 import { checksumsInit } from './utils/checksums';
 import { isNeedToCreateSharpBinaryFile } from "./library/create-sharp-node";
-import { showTimeInStatusBar } from './time';
+import { showTimeInStatusBar, stopTimeInStatusBar } from './time';
 
 export function activate(context: ExtensionContext) {
 	// webview模块内扩展上下文实例赋值
@@ -14,11 +14,12 @@ export function activate(context: ExtensionContext) {
 	checksumsInit();
 	// 注册背景图侧栏页面
 	registBackground();
-
+	// 打开时间显示
 	showTimeInStatusBar(context);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {
+	stopTimeInStatusBar();
 	contextContainer.instance = void 0;
 }

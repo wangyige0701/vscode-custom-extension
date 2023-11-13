fb97dfedb93c6a46b4f3ec8927e51c4d0917f45b
王一阁 <1776686515@qq.com>
2023-11-13 22:37:12 +0800

    打包插件路径规则判断方法优化

--- rollup-plugin/block-bundle/index.js
+++ rollup-plugin/block-bundle/index.js
@@ -1,13 +1,38 @@
-/** @typedef {{ rules: string[] }} Rules */
+/** @typedef {{ from: string, to: string }[]} Rules */
 /** @typedef {{ worker: Rules, package: Rules }} MainOptions */
 
 const { rollup } = require('rollup');
+const Regist = require('./parse');
+const path = require('path');
 
 /**
  * @param {MainOptions} options 
  */
 function main (options) {
-    const { worker: { rules: workerRules = [] }, package: { rules: packageRules = [] } } = options;
+    const regist = new Regist();
+    const { worker = [], package = [] } = options;
+    worker.forEach(item => {
+        regist.add(item, 'worker');
+    });
+    package.forEach(item => {
+        regist.add(item, 'package');
+    });
+
+    // regist.is(path.resolve('src/app/background/time/index.ts'), 'package');
 }
+
+main({
+    worker: [{
+        from: 'src/worker/**',
+        to: 'dist/worker'
+    }],
+    package: [{
+        from: 'src/app/{name}/**',
+        to: 'dist/app/{name}.js'
+    }, {
+        from: 'src/{name}?app,extension.ts,uninstall.ts/**',
+        to: 'dist/{name}.js'
+    }]
+});
 
 module.exports = main;
---
+++ rollup-plugin/utils/relative-position.js
@@ -0,0 +1,34 @@
+
+const splitPath = /([^\/\\]*)(?:\/|\\|\\\\)?/g;
+
+/**
+ * @param {string} path
+ */
+function _splitPath (path) {
+    return [...path.matchAll(splitPath)].map(item => item[1]).filter(item => item);
+}
+
+/**
+ * 生成目标路径相对于引用源文件的相对路径
+ * @param {string} target 被修改的路径
+ * @param {string} source 源文件路径
+ */
+function getRelativePosition (target, source) {
+    const targetPath = _splitPath(target);
+    const sourcePath = _splitPath(source);
+    const relative = [], result = [];
+    for (let i = 0; i < targetPath.length; i++) {
+        if (i >= sourcePath.length) {
+            break;
+        }
+        if (targetPath[i] !== sourcePath[i]) {
+            if (i === 0) {
+                throw new Error("The start of target path is different to source");
+            }
+            relative.push(...new Array(targetPath.length - 1 - i).fill(".."));
+            result.push(...sourcePath.slice(i));
+            break;
+        }
+    }
+    return relative.join("/") + "/" + result.join("/");
+}

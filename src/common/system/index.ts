26f9d536a50fb2f40b5ea364215cb203e9155ea7
WangYige <1776686515@qq.com>
2023-11-27 18:56:18 +0800

    系统模块框架调整

--- src/common/system/index.ts
+++ src/common/system/index.ts
@@ -1,17 +1,3 @@
-import { commands } from "vscode";
-
 export * from "./extension";
-
-/** 重启窗口 */
-export function windowReload () {
-    commands.executeCommand('workbench.action.reloadWindow');
-}
-
-/** 获取node主模块文件路径 */
-export function getNodeModulePath (): string {
-    const module = require.main;
-    if (!module) {
-        return '';
-    }
-    return module.filename;
-}
+export * from "./command";
+export * from "./modulePath";

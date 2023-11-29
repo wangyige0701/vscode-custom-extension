6bd84f05e9960f2478b5120935b63cb00dd2d3ee
WangYige <1776686515@qq.com>
2023-11-29 14:25:37 +0800

    图片压缩方法调整

---
+++ src/app/background/config/index.ts
@@ -0,0 +1,10 @@
+
+/** 图片压缩模块配置项 */
+export function compressConfig () {
+    return {
+        /** 缩略图的存放文件 */
+        compressFileName: 'back.min.wyg',
+        /** 缩略图存放文件夹 */
+        compressFolderName: 'compression'
+    };
+}

b1fa2ee4c302e29de3f275ad2dec2dda276c0988
WangYige <1776686515@qq.com>
2023-12-18 10:43:44 +0800

    配置文件调整，保存时修改格式

--- src/app/background/check/css/setting.ts
+++ src/app/background/check/css/setting.ts
@@ -1,45 +1,45 @@
 /** @fileoverview 校验是否是设置的背景图 */
 
-import { $rej } from "@/error";
-import { getExternalCssFileContent, getExternalCssInfo } from "@background/css/getter/external";
+import { $rej } from '@/error';
+import { getExternalCssFileContent, getExternalCssInfo } from '@background/css/getter/external';
 
 /**
  * 检查指定code是否是当前设置背景图的code
  * @param codeValue
  * @returns 如果state为false时也传了code，则此code是最新需要被设置的图片哈希码
  */
-export function checkIsSettingImage (codeValue: string): Promise<{
-    state: boolean;
-    code?: string;
+export function checkIsSettingImage(codeValue: string): Promise<{
+	state: boolean;
+	code?: string;
 }> {
-    return new Promise((resolve, reject) => {
-        if (!codeValue) {
-            return resolve({
-                state: false
-            });
-        }
-        getExternalCssFileContent()
-        .then(content => {
-            return getExternalCssInfo(content[0]);
-        })
-        .then(data => {
-            if (data) {
-                const { ImageCode } = data;
-                // 如果和上一次是一个哈希值，不再更新数据
-                if (ImageCode === codeValue) {
-                    return resolve({
-                        state: true,
-                        code: ImageCode
-                    });
-                }
-            }
-            resolve({
-                state: false,
-                code: codeValue
-            });
-        })
-        .catch(err => {
-            reject($rej(err, checkIsSettingImage.name));
-        });
-    });
+	return new Promise((resolve, reject) => {
+		if (!codeValue) {
+			return resolve({
+				state: false,
+			});
+		}
+		getExternalCssFileContent()
+			.then(content => {
+				return getExternalCssInfo(content[0]);
+			})
+			.then(data => {
+				if (data) {
+					const { ImageCode } = data;
+					// 如果和上一次是一个哈希值，不再更新数据
+					if (ImageCode === codeValue) {
+						return resolve({
+							state: true,
+							code: ImageCode,
+						});
+					}
+				}
+				resolve({
+					state: false,
+					code: codeValue,
+				});
+			})
+			.catch(err => {
+				reject($rej(err, checkIsSettingImage.name));
+			});
+	});
 }
